588b57d01c0e705b6edd86b8dcd85a8dead100b5
Utkarsh Gupta <emailtoutkarshgupta@gmail.com>
2023-11-19 16:23:15 -0800

    SPI user config options writing & register bit def

--- drivers/inc/stm32f407xx_spi_driver.h
+++ drivers/inc/stm32f407xx_spi_driver.h
@@ -10,11 +10,12 @@ typedef struct
 {
     uint8_t SPI_DeviceMode;
     uint8_t SPI_BusConfig;
+    uint8_t SPI_SclkSpeed;
     uint8_t SPI_DFF;
     uint8_t SPI_CPHA;
     uint8_t SPI_CPOL;
     uint8_t SPI_SSM;
-    uint8_t SPI_speed;
+    
 } SPI_Config_t;
 
 /*
@@ -25,6 +26,51 @@ typedef struct{
     SPI_Config_t SPIConfig;
 } SPI_Handle_t;
 
+/*
+ *@SPI_DeviceMode 
+*/
+#define SPI_DEVICE_MODE_MASTER 1
+#define SPI_DEVICE_MODE_SLAVE 0
+/*
+ * SPI_BusConfig
+*/
+#define SPI_BUSCONFIG_FD 1
+#define SPI_BUSCONFIG_HD 2
+#define SPI_BUSCONFIG_SIMPLEX_RXONLY 3
+
+/*
+ *@SPI_SclkSpeed 
+*/
+#define SPI_SCLKSPEED_DIV2 0
+#define SPI_SCLKSPEED_DIV4 1
+#define SPI_SCLKSPEED_DIV8 2
+#define SPI_SCLKSPEED_DIV16 3
+#define SPI_SCLKSPEED_DIV32 4
+#define SPI_SCLKSPEED_DIV64 5
+#define SPI_SCLKSPEED_DIV128 6
+#define SPI_SCLKSPEED_DIV256 7
+/*
+ *@SPI_DFF 
+*/
+#define SPI_DFF_8BITS 0
+#define SPI_DFF_16BITS 1
+
+/*
+ *CPHA
+*/
+#define SPI_CPHA_HIGH 1
+#define SPI_CPHA_LOW 0
+/*
+ *@CPOL
+ */
+#define SPI_CPOL_HIGH 1
+#define SPI_CPOL_LOW 0
+
+/*
+* @SPI_SSM
+*/
+#define SPI_SSM_EN 1
+#define SPI_SSM_DI 0
 /*********************************************************************
  * Api Supported by this driver
  * *******************************************************************/
